bbd71a30263c67d9d5120b31f62c9cc14550fcd3
NFrhna <bilafarhana15@gmail.com>
2025-01-02 22:50:15 +0800

    change minor issue / add super.

--- src/Bank/StudentAccount.java
+++ src/Bank/StudentAccount.java
@@ -9,11 +9,9 @@ String  institutionName;
 
 public StudentAccount(String name, double balance ,String  institutionName) {
 	super(name, balance, 20000);
-	min_balance=100;
+	super.min_balance=100;
 	this.institutionName=institutionName;
-//	this.type="Stuedent Account";
+//	this.type="Student Account";
 }
-
-
 
 }
